886c9a9b82d288d7e7959c0e4e21f8ba65b18709
hardca <143009919+hardca@users.noreply.github.com>
2023-08-23 10:34:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.header("Breakfast Menu")
+streamlit.header('Breakfast Menu')
 
-streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
-streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
-streamlit.text("🐔Hard-Boiled Free-Range Egg")
-streamlit.text("🥑🍞Avocado Toast")
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
