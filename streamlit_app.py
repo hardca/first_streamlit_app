c9695cb42a9c62abb0f3604f52e4164cebcf4944
hardca <143009919+hardca@users.noreply.github.com>
2023-08-23 10:26:12 -0500

    Update streamlit_app.py
    changed code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit
 
-streamlit.title("My parents new healthy diner")
-
-streamlit.header("BREAKFAST MENU")
-streamlit.text("Eggs")
-streamlit.text("Bacon")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
