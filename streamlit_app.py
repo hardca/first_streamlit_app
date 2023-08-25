0bffab3a08ec55f2ed566c9150390bcb2aa07ca2
hardca <143009919+hardca@users.noreply.github.com>
2023-08-25 14:48:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 #import snowflake.connector
 
-streamlit.header("The fruitload list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!:")
 #snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -63,7 +63,7 @@ def get_fruit_load_list():
          return my_cur.fetchall()
 
 #add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
    my_cnx.close()
