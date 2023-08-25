d349e37ef9ee6eb72794f4ec5baa2cafd37cd01f
hardca <143009919+hardca@users.noreply.github.com>
2023-08-25 12:23:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,11 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruitload list contains:")
 streamlit.dataframe(my_data_rows)
 
+streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thanks for adding', fruit_choice)
+
+
 
 
 
